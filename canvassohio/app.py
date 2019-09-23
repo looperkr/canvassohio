4c5040f8961d8b72faf4bb3967f189ea64f6d1ff
Kristina Looper <looper.kristina@gmail.com>
2019-09-23 17:12:36 -0400

    updated Procfile

--- canvassohio/app.py
+++ canvassohio/app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from flask import Flask, render_template, request, send_file, Response, redirect
 from wtforms import Form, TextAreaField, validators
 import geopandas as gpd
